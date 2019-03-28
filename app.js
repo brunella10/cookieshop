fad78cda3295d6d01d90a74592c261c65b657505
brunella10 <gaby1010@gmail.com>
2019-03-28 15:52:07 -0700

    Starting cookie shop

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function shopCookie (chocolate, vanilla, oatmeal){
+    // var cookie = prompt ('Today we have ' +chocolateCookie+ ' Chocolate Cookies ' +vanillaCookie+ 'Vanilla cookies and ' +oatmealCookie+ 'oatmeal cookies to sale! ');
+    var chocolateCookie = 10;
+    var vanillaCookie = 20;
+    var oatmealCookie = 35;
+alert ('Today we have ' +chocolateCookie+ ' Chocolate Cookies ' +vanillaCookie+ 'Vanilla cookies and ' +oatmealCookie+ 'oatmeal cookies to sale! ')
+};
+cookie();
